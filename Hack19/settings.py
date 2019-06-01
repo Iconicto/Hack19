b808b5c4139b09f9e7f45eb31b13011b53d20405
Isala Piyarisi <code@isala.me>
2019-06-01 12:52:05 +0530

    I have no idea what I'm doing here.

--- Hack19/settings.py
+++ Hack19/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',
-    'Feed',
+    'Hack19.Feed',
     'storages',
     'corsheaders',
     'django_filters',
