f539aa067368e2c232cfc29cea318512c49228ff
Isala Piyarisi <code@isala.me>
2019-06-01 13:59:08 +0530

    I honestly wish I could remember what was going on here...

--- Feed/schema.py
+++ Feed/schema.py
@@ -6,5 +6,34 @@ from django.db.models import Q
 from .models import *
 
 
+class NewsType(DjangoObjectType):
+    class Meta:
+        model = News
+
+
+class EventType(DjangoObjectType):
+    class Meta:
+        model = Event
+
+
+class TutorialType(DjangoObjectType):
+    class Meta:
+        model = Tutorial
+
+
+class CommunityType(DjangoObjectType):
+    class Meta:
+        model = Community
+
+
+class WidgetType(DjangoObjectType):
+    class Meta:
+        model = Community
+
+
 class Query(graphene.ObjectType):
-    pass
+    news = graphene.List(NewsType)
+    events = graphene.List(EventType)
+    tutorials = graphene.List(TutorialType)
+    communities = graphene.List(CommunityType)
+    widget = graphene.List(WidgetType)
