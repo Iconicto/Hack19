94d4eb5f57a43fd133b7cc3794cab4626151c626
Isala Piyarisi <code@isala.me>
2019-06-01 13:25:41 +0530

    Fixed the build.

--- Feed/models.py
+++ Feed/models.py
@@ -26,7 +26,7 @@ class Event(models.Model):
     name = models.CharField(max_length=150)
     venue = models.CharField(max_length=350)
     date = models.DateField()
-    time = models.DateTimeField()
+    time = models.TimeField()
     description = models.TextField(blank=True, null=True)
     registration_link = models.URLField()
 
